23606961273c29c94b7e85dff499bc618bee8443
Juan Martín García <hello@juangarcia.com.ar>
2021-04-15 11:20:35 -0300

    Added reglas for chinchon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import FooterStyles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={FooterStyles.FooterContainer}>
-      <p className={FooterStyles.FooterText}>Copyright &copy; {new Date().getFullYear()}. Created by <a href="https://juangarcia.design" className={FooterStyles.FooterLink}>JMG</a>.</p>
+      <p className={FooterStyles.FooterText}>Copyright &copy; {new Date().getFullYear()}. Created by <a href="https://juangarcia.design" className={FooterStyles.FooterLink}>JMG</a>. Pod&eacute;s revisar las reglas del chinch&oacute;n <a href="https://www.mundijuegos.com.ar/multijugador/chinchon/reglas/" className={FooterStyles.FooterLink}>ac&aacute;.</a></p>
     </footer>
   );
 };
